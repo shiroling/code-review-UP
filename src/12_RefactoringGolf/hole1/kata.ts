0de2428b1dd96585c3ef3f9495eae48d2c4a235a
Shiroling <contact.shiroling@gmail.com>
2023-12-01 12:28:21 +0100

    Correctifs type loi de demeter, Tile devient une classe à part entière pour favoriser sa responsabilité et sa logique interne + un peu de naming

--- src/12_RefactoringGolf/hole1/kata.ts
+++ src/12_RefactoringGolf/hole1/kata.ts
@@ -7,25 +7,33 @@ const firstColumn = 0;
 const secondColumn = 1;
 const thirdColumn = 2;
 
-const playerO = 'O';
+const symbolPlayerO = 'O';
 const emptyPlay = ' ';
 
 export class Game {
   private _lastSymbol = emptyPlay;
   private _board: Board = new Board();
 
-  public Play(symbol: string, x: number, y: number): void {
+  public Play(symbol: string, colone: number, ligne: number): void {
     this.validateFirstMove(symbol);
     this.validatePlayer(symbol);
-    this.validatePositionIsEmpty(x, y);
+    this.validatePositionIsEmpty(colone, ligne);
 
     this.updateLastPlayer(symbol);
-    this.updateBoard(symbol, x, y);
+    this.updateBoard(symbol, colone, ligne);
   }
 
-  private validateFirstMove(player: string) {
-    if (this._lastSymbol == emptyPlay) {
-      if (player == playerO) {
+  private isFirstMove(): boolean{
+    return this._lastSymbol == emptyPlay
+  }
+
+  private isFirstPlayerSymbol(symbol: string): boolean{
+    return symbol == symbolPlayerO
+  }
+
+  private validateFirstMove(playerSymbol: string) {
+    if (this.isFirstMove()) {
+      if (this.isFirstPlayerSymbol(playerSymbol)) {
         throw new Error('Invalid first player');
       }
     }
@@ -37,8 +45,8 @@ export class Game {
     }
   }
 
-  private validatePositionIsEmpty(x: number, y: number) {
-    if (this._board.TileAt(x, y).Symbol != emptyPlay) {
+  private validatePositionIsEmpty(colone: number, ligne: number) {
+    if (!this._board.isEmptyAt(colone,ligne)) {
       throw new Error('Invalid position');
     }
   }
@@ -47,21 +55,21 @@ export class Game {
     this._lastSymbol = player;
   }
 
-  private updateBoard(player: string, x: number, y: number) {
-    this._board.AddTileAt(player, x, y);
+  private updateBoard(player: string, colone: number, ligne: number) {
+    this._board.AddTileAt(player, colone, ligne);
   }
 
-  public Winner(): string {
+  public Winner(): String {
     if (this.isRowFull(firstRow) && this.isRowFullWithSameSymbol(firstRow)) {
-      return this._board.TileAt(firstRow, firstColumn)!.Symbol;
+      return this._board.getSymbolAt(firstRow, firstColumn);
     }
 
     if (this.isRowFull(secondRow) && this.isRowFullWithSameSymbol(secondRow)) {
-      return this._board.TileAt(secondRow, firstColumn)!.Symbol;
+      return this._board.getSymbolAt(secondRow, firstColumn);
     }
 
     if (this.isRowFull(thirdRow) && this.isRowFullWithSameSymbol(thirdRow)) {
-      return this._board.TileAt(thirdRow, firstColumn)!.Symbol;
+      return this._board.getSymbolAt(thirdRow, firstColumn);
     }
 
     return emptyPlay;
@@ -69,25 +77,56 @@ export class Game {
 
   private isRowFull(row: number) {
     return (
-      this._board.TileAt(row, firstColumn)!.Symbol != emptyPlay &&
-      this._board.TileAt(row, secondColumn)!.Symbol != emptyPlay &&
-      this._board.TileAt(row, thirdColumn)!.Symbol != emptyPlay
+      !this._board.isEmptyAt(row, firstColumn) &&
+      !this._board.isEmptyAt(row, secondColumn) &&
+      !this._board.isEmptyAt(row, thirdColumn)
     );
   }
 
   private isRowFullWithSameSymbol(row: number) {
     return (
-      this._board.TileAt(row, firstColumn)!.Symbol ==
-        this._board.TileAt(row, secondColumn)!.Symbol &&
-      this._board.TileAt(row, thirdColumn)!.Symbol == this._board.TileAt(row, secondColumn)!.Symbol
+      this._board.getTilelAt(row, firstColumn).equals(this._board.getTilelAt(row, secondColumn)) 
+      &&
+      this._board.getTilelAt(row, secondColumn).equals(this._board.getTilelAt(row, thirdColumn))
     );
   }
 }
 
-interface Tile {
-  X: number;
-  Y: number;
-  Symbol: string;
+class Tile {
+  private colone: number;
+  private ligne: number;
+  private symbol: string;
+
+  constructor(colone: number, ligne: number, symbol: string) {
+    this.colone= colone;
+    this.ligne = ligne;
+    this.symbol = symbol
+  }
+
+  public getColone(): number {
+    return this.colone
+  }
+
+  public getligne() : number{
+    return this.ligne
+  }
+
+  public getSymbol() : string{
+    return this.symbol
+  }
+
+  public setSymbol(symbol : string){
+      this.symbol = symbol
+  }
+
+  public hasSymbol(symbol: string) {
+    return this.symbol == symbol
+  }
+  
+  public equals(tile: Tile) {
+    return this.symbol == tile.getSymbol()
+  }
+
 }
 
 class Board {
@@ -96,17 +135,29 @@ class Board {
   constructor() {
     for (let i = firstRow; i <= thirdRow; i++) {
       for (let j = firstColumn; j <= thirdColumn; j++) {
-        const tile: Tile = { X: i, Y: j, Symbol: emptyPlay };
+        const tile = new Tile( i, j,emptyPlay );
         this._plays.push(tile);
       }
     }
   }
 
-  public TileAt(x: number, y: number): Tile {
-    return this._plays.find((t: Tile) => t.X == x && t.Y == y)!;
+  public isSymbolAt(symbol: string, colone: number, ligne: number): Boolean {
+    return this.getSymbolAt(colone, ligne) == symbol; 
   }
 
-  public AddTileAt(symbol: string, x: number, y: number): void {
-    this._plays.find((t: Tile) => t.X == x && t.Y == y)!.Symbol = symbol;
+  public isEmptyAt(colone: number, ligne: number): Boolean {
+    return this.isSymbolAt(emptyPlay, colone, ligne); 
+  }
+
+  public getTilelAt(colone: number, ligne: number): Tile {
+    return this._plays.find((t: Tile) => t.getColone() == colone && t.getligne() == ligne)!;
+  }
+
+  public getSymbolAt(colone: number, ligne: number): String {
+    return this.getTilelAt(colone, ligne).getSymbol();
+  }
+
+  public AddTileAt(symbol: string, colone: number, ligne: number): void {
+    this._plays.find((t: Tile) => t.getColone() == colone && t.getligne() == ligne)?.setSymbol(symbol);
   }
 }
